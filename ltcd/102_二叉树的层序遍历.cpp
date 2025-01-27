3a8c489dd91231c21adb8424fe09a5ad601df644
kodaniii <436073092@qq.com>
2025-01-27 21:15:45 +0800

    update 102 226

---
+++ ltcd/102_二叉树的层序遍历.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> thisLevel;
+        queue<TreeNode*> q;
+        if(root) q.push(root);
+        //队列非空
+        while (!q.empty()) {
+            //记录当前层的size()
+            int size = q.size();
+            //清空thisLevel
+            thisLevel.clear();
+            while (size--) {
+                TreeNode* node = q.front();
+                q.pop();
+                thisLevel.push_back(node->val);
+                if(node->left) q.push(node->left);
+                if(node->right) q.push(node->right);
+            }
+            res.push_back(thisLevel);
+        }
+        return res;
+    }
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
