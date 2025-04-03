1937276613fdd0f293a351b2f510ce5ff8f0470b
kodaniii <436073092@qq.com>
2025-04-03 20:56:33 +0800

    update 1035 53(DP)

--- ltcd/1143_最长公共子序列.cpp
+++ ltcd/1143_最长公共子序列.cpp
@@ -12,7 +12,7 @@ public:
         int res = INT_MIN;
         for (int i = 1; i <= text1.size(); i++) {
             for (int j = 1; j <= text2.size(); j++) {
-                if (text1[i - 1] == text2[j - 1]) dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + 1);
+                if (text1[i - 1] == text2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
                 else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
                 res = max(res, dp[i][j]);
             }
--- ltcd/53_最大子数组和.cpp
+++ ltcd/53_最大子数组和.cpp
@@ -3,6 +3,26 @@
 
 using namespace std; 
 
+/*DP*/
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        if (nums.size() == 0) return 0;
+        //dp[i]：nums[0-i]当前下标子数组和
+        vector<int> dp(nums.size());
+        //初始化
+        dp[0] = nums[0];
+        int max_sum = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 1] + nums[i], nums[i]);
+            // if(dp[i - 1] < 0) dp[i] = nums[i]; 
+            // else dp[i] = dp[i - 1] + nums[i];
+            max_sum = max(dp[i], max_sum);
+        }
+        return max_sum;
+    }
+};
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
